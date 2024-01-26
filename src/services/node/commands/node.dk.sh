47de98f88f5cb5e8f927b8afee2710ed5e4420a4
Łukasz Zaroda <luken@luken-tech.pl>
2024-01-26 12:08:52 +0100

    Fixed wrong host uid/gid references in the node script.

--- src/services/node/commands/node.dk.sh
+++ src/services/node/commands/node.dk.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-docker run -it --rm -w=/var/node -u="$(id -u "${USER}"):$(id -g "${USER}")" -v "${DRAKY_PROJECT_ROOT}/${NODE_PACKAGE_JSON_PATH}:/var/node/:cached,rw" "${NODE_DOCKER_IMAGE}" "$@"
+docker run -it --rm -w=/var/node -u="${DRAKY_HOST_UID}:${DRAKY_HOST_GID}" -v "${DRAKY_PROJECT_ROOT}/${NODE_PACKAGE_JSON_PATH}:/var/node/:cached,rw" "${NODE_DOCKER_IMAGE}" "$@"
