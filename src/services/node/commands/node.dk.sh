bb333cab2da45c87b7493f3f713fc57d12fe0546
Łukasz Zaroda <luken@luken-tech.pl>
2025-01-09 21:33:02 +0100

    Made "node" script to be able to run in noninteractive mode.

--- src/services/node/commands/node.dk.sh
+++ src/services/node/commands/node.dk.sh
@@ -1,3 +1,12 @@
 #!/usr/bin/env bash
 
-docker run -it --rm -w=/var/node -u="${DRAKY_HOST_UID}:${DRAKY_HOST_GID}" -v "${DRAKY_PROJECT_ROOT}/${NODE_PACKAGE_JSON_PATH}:/var/node/:cached,rw" -p"${NODE_WEBPACK_LIVE_RELOAD_PORT}:35729" "${NODE_DOCKER_IMAGE}" "$@"
+ARGS=(
+  -i
+)
+
+if [[ "$NONINTERACTIVE" != 1 ]]; then
+  ARGS+=(-t)
+  ARGS+=(-p"35729:35729")
+fi
+
+docker run "${ARGS[@]}" --rm -w=/var/node -u="${DRAKY_HOST_UID}:${DRAKY_HOST_GID}" -v "${DRAKY_PROJECT_ROOT}/${NODE_PACKAGE_JSON_PATH}:/var/node:cached,rw" "${NODE_DOCKER_IMAGE}" "$@"
