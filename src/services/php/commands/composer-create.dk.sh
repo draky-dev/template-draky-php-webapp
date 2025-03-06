bd51b1f2f668691eda58e01d8d5d5d3300b50225
Łukasz Zaroda <luken@luken-tech.pl>
2025-03-06 20:03:17 +0100

    Changed the `composer-create` command to be run in a separate container.

---
+++ src/services/php/commands/composer-create.dk.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+
+TEMP_PATH=/tmp/composer-create
+PROJECT_PATH=${DRAKY_PHP_CONTAINER_ROOT}
+
+COLOR_RESET='\033[0m'
+COLOR_GREEN='\033[0;92m'
+COLOR_RED='\033[0;91m'
+
+ARGS=(
+  -i
+)
+
+if [ -z "$1" ]; then
+  printf "%b\nERROR: Package as an argument is required%b" "${COLOR_RED}" "${COLOR_RESET}"
+  exit 1;
+fi
+
+docker run "${ARGS[@]}" --rm -w="${DRAKY_PHP_CONTAINER_ROOT}" -u="${DRAKY_HOST_UID}:${DRAKY_HOST_GID}" -v "${DRAKY_PROJECT_ROOT}:${DRAKY_PHP_CONTAINER_ROOT}:cached,rw" "$DRAKY_COMPOSER_IMAGE" sh -s << EOF
+composer create-project "$@" "${TEMP_PATH}"
+
+for file in "$PROJECT_PATH"/*; do
+   if [ -f "$TEMP_PATH/\${file##*/}" ]; then
+      printf "%b\nERROR: Conflicting files exist in the project%b" "${COLOR_RED}" "${COLOR_RESET}"
+      cleanup
+      exit 1
+   fi
+done
+
+cp -rnP ${TEMP_PATH}/* "${PROJECT_PATH}"
+
+printf "%b\nSUCCESS: Project has been created.%b" "${COLOR_GREEN}" "${COLOR_RESET}"
+EOF
